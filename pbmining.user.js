4c0a3ac507383d3fb6edf00d1a9d5ddcb6239200
Laurence Putra Franslay <laurenceputra@gmail.com>
2014-09-16 22:50:49 +0800

    Update pbmining.user.js

--- pbmining.user.js
+++ pbmining.user.js
@@ -1,3 +1,11 @@
+// ==UserScript==
+// @name        pbmining
+// @namespace   pbmining
+// @description avoids the pbmining stats page
+// @include     https://pbmining.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
 if(window.location == "https://pbmining.com/stats.html" || window.location == "https://pbmining.com/stats.html?&ref="){
 	window.location = "https://pbmining.com/profile.html?";
 }
